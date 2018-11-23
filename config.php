5f6a415e6fc49d5d3c7e6c70ca11fb5ffa598a15
nimesh <32710905+nimesh10patil@users.noreply.github.com>
2018-11-23 17:22:11 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 	session_start();
 	require_once "GoogleAPI/vendor/autoload.php";
 	$gClient = new Google_Client();
-	$gClient->setClientId("84553192998-vuo9ejmdlicpnvje57mi24vt4apfglui.apps.googleusercontent.com");
-	$gClient->setClientSecret("fWDYweK8FhqKwX3IWVHmwKld");
+	$gClient->setClientId("your client id);
+	$gClient->setClientSecret("your client secret");
 	$gClient->setApplicationName("CPI Login Tutorial");
 	$gClient->setRedirectUri("http://localhost/Litap/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
